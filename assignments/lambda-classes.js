2ce22a2fde55b469bddd544dfdd9adec2faffa9f
PascalUlor <pascalulor@yahoo.com>
2019-05-09 14:33:18 +0100

    ft(create person class) create person class and instantiate objects of the class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+/**
+ * Person Class
+ */
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.gender = attributes.gender;
+        this.location = attributes.location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+const fred = new Person({name: 'Fred', age: 12, gender: 'male', location: 'Bedrock'});
+const pascal = new Person({name: 'Pascal', age: 92, gender: 'male', location: 'Lagos'});
+console.log(fred.speak());
+console.log(pascal.speak());
