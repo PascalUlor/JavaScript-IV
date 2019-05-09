f4bb60669ad8246be24f5d8a76fa84e9ef096f18
PascalUlor <pascalulor@yahoo.com>
2019-05-09 15:46:01 +0100

    ft(finish mvp) create project manager class edit return in methods in all class to console log

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,7 +11,7 @@ class Person {
     }
 
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
     }
 }
 
@@ -28,15 +28,16 @@ class Instructor extends Person {
         this.catchPhrase = instructorAttr.catchPhrase
     }
     demo(subject) {
-        return `Today we are learning about ${subject}`
+        console.log(`Today we are learning about ${subject}`)
     }
     grade(student, subject){
-        return `${student.name} receives a perfect score on ${subject}`
+        console.log(`${student.name} receives a perfect score on ${subject}`)
     }
 }
 
 const joe = new Instructor(
-    {name: 'Joe',
+    {
+    name: 'Joe',
     age: 12, 
     gender: 'male', 
     location: 'Bedrock',
@@ -60,13 +61,15 @@ class Student extends Person {
     }
 
     listsSubjects() {
-        return ` a method that logs out all of the student favoriteSubjects one by one`
+        this.favSubjects.forEach(subject => {
+            console.log(subject);
+        });
     }
     PRAssignment(subject) {
-        return `a method that receives a subject as an argument and logs out that the ${this.name} has submitted a PR for ${subject}`
+        console.log(`a method that receives a subject as an argument and logs out that the ${this.name} has submitted a PR for ${subject}`)
     }
     sprintChallenge(subject){
-        return `similar to PRAssignment but logs out ${this.name} has begun sprint challenge on ${subject}`
+        console.log(`similar to PRAssignment but logs out ${this.name} has begun sprint challenge on ${subject}`)
     }
 
 }
@@ -78,10 +81,42 @@ const johnson = new Student({
     location: 'Bedrock',
     previousBackground: 'Data science background',
     className: 'Math 405',
-    favSubjects: 'Java'
+    favSubjects: ['Java', 'CSS', 'ReactJS', 'Python']
 })
 
 console.log(johnson);
 console.log(johnson.listsSubjects());
 console.log(johnson.PRAssignment('Java'));
 console.log(johnson.sprintChallenge('Python'));
+
+class ProjectManager extends Instructor {
+    constructor(pmAttr) {
+        super(pmAttr)
+        this.gradClassName = pmAttr.gradClassName;
+        this.favInstructor = pmAttr.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standup times!​​​​​`)
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+    }
+}
+
+const matt = new ProjectManager({
+    name: 'Matt',
+    age: 22, 
+    gender: 'male', 
+    location: 'Bedrock',
+    specialty: 'Data Science',
+    favLanguage: 'Python',
+    catchPhrase: 'In God We trust',
+    gradClassName: 'WEBEU2',
+    favInstructor: 'Gabe'
+})
+
+console.log(matt);
+console.log(matt.standUp('WebSprint'));
+console.log(matt.debugsCode(johnson, 'Javascript'));
